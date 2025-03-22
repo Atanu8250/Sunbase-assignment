113b70f9c3a1c1526bddb186a265cf212ef10b83
Atanu8250 <atanu20uparsole@gmail.com>
2025-03-22 16:54:20 +0530

    form edit working like google form

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ let DefaultformData = [
           type: "checkbox",
           label: "Sample Checkbox",
      },
-]
+];
 
 
 
@@ -56,7 +56,7 @@ function setTheme(theme) {
 }
 
 function showPreview() {
-     console.log(modal.style)
+     // generateFormPreview();
      modal.style.display = 'block';
 }
 
@@ -64,43 +64,21 @@ function closePreview() {
      modal.style.display = 'none';
 }
 
-function renderFormElement(field, preview = false) {
+function renderFormPreviewElement(field) {
      switch (field.type) {
           case 'input':
                return (
-                    `<div class="field-box">
-                         <div class="field-info">
-                              <label for='${field.id}' class="field-label">${field.label}</label>
-
-                              <span class="field-action-btns ${preview && 'hide'}">
-                                   <button class="edit">
-                                        <i class="edit fa-regular fa-pen-to-square"></i>
-                                   </button>
-                                   <button class="delete" onclick="deleteElement('${field.id}')">
-                                        <i class="delete fa-solid fa-trash"></i>
-                                   </button>
-                              </span>
-                         </div>
-                         <input id='${field.id}' type="text" placeholder="${field.placeholder}" class="field" />
+                    `<div class="preview-field-box">
+                         <label for='${field.id}' class="preview-field-label">${field.label}</label>
+                         <input id='${field.id}' type="text" placeholder="${field.placeholder}" class="preview-field" />
                     </div>`
                )
 
           case 'select':
                return (
-                    `<div class="field-box">
-                         <div class="field-info">
-                              <label for='${field.id}' class="field-label">${field.label}</label>
-
-                              <span class="field-action-btns ${preview && 'hide'}">
-                                   <button class="edit">
-                                        <i class="edit fa-regular fa-pen-to-square"></i>
-                                   </button>
-                                   <button class="delete" onclick="deleteElement('${field.id}')">
-                                        <i class="delete fa-solid fa-trash"></i>
-                                   </button>
-                              </span>
-                         </div>
-                         <select id='${field.id}' class="field">
+                    `<div class="preview-field-box">
+                         <label for='${field.id}' class="preview-field-label">${field.label}</label>
+                         <select id='${field.id}' class="preview-field">
                               ${field.options.map(opt => `<option value="${opt.split(' ').join('-')}">${opt}</option>`).join('')}
                          </select>
                     </div>`
@@ -108,40 +86,19 @@ function renderFormElement(field, preview = false) {
 
           case 'textarea':
                return (
-                    `<div class="field-box">
-                         <div class="field-info">
-                              <label for='${field.id}' class="field-label">${field.label}</label>
-
-                              <span class="field-action-btns ${preview && 'hide'}">
-                                   <button class="edit">
-                                        <i class="edit fa-regular fa-pen-to-square"></i>
-                                   </button>
-                                   <button class="delete" onclick="deleteElement('${field.id}')">
-                                        <i class="delete fa-solid fa-trash"></i>
-                                   </button>
-                              </span>
-                         </div>
-                         <textarea id='${field.id}' placeholder="${field.placeholder}" class="field"></textarea>
+                    `<div class="preview-field-box">
+                         <label for='${field.id}' class="preview-field-label">${field.label}</label>
+                         <textarea id='${field.id}' placeholder="${field.placeholder}" class="preview-field"></textarea>
                     </div>`
                )
 
           case 'checkbox':
                return (
-                    `<div class="field-box">
-                         <div class="field-info">
-                              <div class="checkbox">
-                                   <input id='${field.id}' type="checkbox" class="checkbox-field" />
-                                   <label for='${field.id}' class="field-label">${field.label}</label>
+                    `<div class="preview-field-box">
+                              <div class="preview-checkbox">
+                                   <input id='${field.id}' type="checkbox" class="preview-checkbox-field" />
+                                   <label for='${field.id}' class="preview-field-label">${field.label}</label>
                               </div>
-
-                              <span class="field-action-btns ${preview && 'hide'}">
-                                   <button class="edit">
-                                        <i class="edit fa-regular fa-pen-to-square"></i>
-                                   </button>
-                                   <button class="delete" onclick="deleteElement('${field.id}')">
-                                        <i class="delete fa-solid fa-trash"></i>
-                                   </button>
-                              </span>
                          </div>
                     </div>`
                )
@@ -151,70 +108,217 @@ function renderFormElement(field, preview = false) {
      }
 }
 
+function generateFormPreview() {
+     previewForm.innerHTML = '';
+
+     const form = document.createElement('form');
+     form.innerHTML = DefaultformData.map(el => renderFormPreviewElement(el)).join("");
+     previewForm.appendChild(form);
+}
+
+function renderFormElements(field) {
+     // Create the main contianer
+     const fieldBox = document.createElement('div');
+     fieldBox.classList.add('field-box');
+
+     // Delete button 
+     const deleteBtn = document.createElement('button');
+     deleteBtn.classList.add('delete', 'field-action-btn');
+     deleteBtn.onclick = () => { deleteElement(field.id) };
+
+     const trashIcon = document.createElement('i');
+     trashIcon.classList.add('fa-solid', 'fa-trash');
+
+     deleteBtn.appendChild(trashIcon);
+     fieldBox.appendChild(deleteBtn);
+
+     // First field group
+     const fieldGroup1 = document.createElement('div');
+     fieldGroup1.classList.add('field-group');
+
+     const label1 = document.createElement('label');
+     label1.classList.add('field-label');
+     label1.textContent = `Enter ${field.type} label`;
+
+     const input1 = document.createElement('input');
+     input1.onchange = (e) => udpateFormData(e, field.id, 'label');
+     input1.type = 'text';
+     input1.placeholder = `Enter ${field.type} label`;
+     input1.classList.add('field');
+     input1.value = field.label;
+
+     fieldGroup1.appendChild(label1);
+     fieldGroup1.appendChild(input1);
+     fieldBox.appendChild(fieldGroup1);
+
+     // --- Second field group (placeholder) ---
+     if (field.type !== 'checkbox') {
+          const fieldGroup2 = document.createElement('div');
+          fieldGroup2.classList.add('field-group');
+
+          const label2 = document.createElement('label');
+          label2.classList.add('field-label');
+
+          fieldGroup2.appendChild(label2);
+
+          if (field.type === 'input' || field.type === 'textarea') {
+               const input2 = document.createElement('input');
+
+               input2.onchange = (e) => udpateFormData(e, field.id, 'placeholder');
+               input2.classList.add('field');
+
+               label2.textContent = `Enter ${field.type} placeholder`;
+               input2.placeholder = `Enter ${field.type} placeholder`;
+               input2.type = 'text';
+               input2.value = field.placeholder || field.options.join(", ");
+
+               fieldGroup2.appendChild(input2);
+          } else if (field.type === 'select') {
+               label2.textContent = `Enter ${field.type} options`;
+
+               let optionsBox = renderOptionFields(field);
+               fieldGroup2.appendChild(optionsBox);
+
+               const addOptionBtn = document.createElement('button');
+               addOptionBtn.classList.add('add-option', 'btn')
+               addOptionBtn.textContent = 'Add Option';
+               addOptionBtn.onclick = () => {
+                    // Form data udpate
+                    DefaultformData.forEach(formData => {
+                         if (formData.id === field.id) {
+                              formData.options.push(`Sample Option`)
+                         }
+                         return formData;
+                    })
+
+                    // update DOM
+                    updateFormCanvas()
+               }
+               fieldGroup2.appendChild(addOptionBtn)
+          }
+
+
+          fieldBox.appendChild(fieldGroup2);
+     }
+
+     return fieldBox;
+
+}
+
+function udpateFormData(event, fieldId, keyType) {
+     const updatedValue = event.target.value;
+
+     DefaultformData.forEach(formData => {
+          if (formData.id === fieldId) {
+               formData[keyType] = updatedValue;
+          }
+     })
+}
+
+function renderOptionFields(field) {
+     const optionsBox = document.createElement('div');
+     optionsBox.classList.add('multiple-options')
+     optionsBox.innerHTML = '';
+
+     field.options.forEach((opt, optInd, arr) => {
+          const singleOption = document.createElement('div');
+          singleOption.classList.add('option-box')
+
+          const option = document.createElement('input');
+          option.onchange = (e) => {
+               const updatedValue = e.target.value;
+
+               DefaultformData.forEach(formData => {
+                    if (formData.id === field.id) {
+                         formData.options[optInd] = updatedValue;
+                    }
+                    return formData;
+               })
+          }
+          option.classList.add('field', 'option-field');
+          option.type = 'text';
+          option.value = opt;
+
+
+          const close = document.createElement('button');
+          close.classList.add('option-close')
+          close.innerHTML = '<i class="fa-solid fa-close"></i>';
+          close.onclick = (e) => {
+               // Form data udpate
+               DefaultformData.forEach(formData => {
+                    if (formData.id === field.id) {
+                         formData.options = formData.options.filter((_, i) => i !== optInd);
+                    }
+                    return formData;
+               })
+               // Remove the node from the DOM
+               e.target.closest('div').remove();
+          };
+          singleOption.append(option, close)
+          optionsBox.appendChild(singleOption);
+     })
+
+     return optionsBox;
+}
+
 function updateFormCanvas(formData = DefaultformData) {
      if (formData.length <= 0) {
           formCanvas.classList.add('empty-state');
           formCanvas.innerHTML = `<h3>Add Components to check the preview here!</h3>`;
-
-          previewForm.classList.add('empty-state');
-          previewForm.innerHTML = `<h3>Add Components to check the preview here!</h3>`;
-
           return;
      }
 
      formCanvas.innerHTML = '';
      formCanvas.classList.remove('empty-state');
-     previewForm.innerHTML = '';
-     previewForm.classList.remove('empty-state');
 
      const formContainer = document.createElement('div')
      formContainer.classList.add('form-container');
      formContainer.innerHTML = '';
 
      formData.forEach((field) => {
-          let fieldBox = renderFormElement(field);
-          formContainer.innerHTML += fieldBox;
-
-          let previewFieldBox = renderFormElement(field, true);
-          previewForm.innerHTML += previewFieldBox;
+          let fieldBox = renderFormElements(field);
+          formContainer.appendChild(fieldBox);
      })
 
      formCanvas.appendChild(formContainer);
+}
+
+function addFormElement(formElementObj) {
+     const element = {
+          id: generateUUID(),
+          type: formElementObj.type,
+          label: formElementObj.label,
+     }
+
+     if (formElementObj.placeholder) element.placeholder = formElementObj.placeholder;
+
+     if (formElementObj.options) element.options = formElementObj.options;
+
+     DefaultformData.push(element);
+     updateFormCanvas();
+}
+
+function deleteElement(id) {
+     DefaultformData = DefaultformData.filter(form => form.id !== id);
+     updateFormCanvas();
 }
 
 function generateUUID() {
      return `${Date.now().toString()}-${Math.random() * 9999}`
 }
 
-function addFormElement(type, label, placeholder, options) {
-     const element = {
-          id: generateUUID(),
-          type,
-          label,
-     }
+function generateFormHTML(html) {
+     const container = document.createElement('div');
+     container.innerHTML = html;
 
-     if (type !== 'checkbox' && type !== 'select') element.placeholder = placeholder;
+     // Remove all 'class' attributes
+     container.querySelectorAll('[class]').forEach(el => el.removeAttribute('class'));
 
-     if (type === 'select') element.options = options;
-
-     console.log('DefaultformData:', DefaultformData)
-     DefaultformData.push(element);
-     updateFormCanvas();
-}
-
-function deleteElement(id) {
-     let labels = document.querySelectorAll(`label[for='${id}']`);
-     labels.forEach(label => label.closest('.field-box').remove())
-     DefaultformData = DefaultformData.filter(form => form.id !== id);
-     if (DefaultformData.length <= 0) updateFormCanvas();
-}
-
-function editElement() {
-
+     return container.innerHTML;
 }
 
 function copyHTML() {
-     const html = previewForm.innerHTML;
+     const html = generateFormHTML(document.getElementById('previewForm').innerHTML);
      navigator.clipboard.writeText(html);
 }
 
@@ -240,36 +344,32 @@ modalContent.addEventListener('click', () => {
 
 componentHeaders.forEach(header => {
      header.addEventListener('click', () => {
-          const item = header.parentElement;
-          item.classList.toggle('expanded');
+          const formElementObj = {
+               type: header.dataset.type,
+               label: `Sample ${header.dataset.type.charAt(0).toUpperCase() + header.dataset.type.slice(1)}`,
+          }
+
+          if (formElementObj.type !== 'checkbox' || formElementObj.type !== 'select') {
+               formElementObj.placeholder = 'Sample Placeholder'
+          }
+
+          if (formElementObj.type === 'select') {
+               formElementObj.options = ['Sample Option', 'Sample Option', 'Sample Option']
+          }
+
+          addFormElement(formElementObj);
      })
 })
 
-componentForms.forEach(form => {
-     form.addEventListener('submit', e => {
-          e.preventDefault();
 
-          const type = e.submitter.dataset.type;
-          const labelInput = form.querySelector('.label-input').value || `Sample ${type.charAt(0).toUpperCase() + type.slice(1)}`;
-          let placeholderORoptions = type !== 'checkbox' && form.querySelector('.placeholder-input').value;
+document.getElementById('saveBtn').onclick = () => {
+     generateFormPreview();
+     console.log(JSON.stringify(DefaultformData));
+}
 
-          if (type === 'select') {
-               const options = placeholderORoptions.split(',').map(opt => opt.trim()).filter(opt => opt);
-               addFormElement(type, labelInput, placeholderORoptions, options.length ? options : ['Sample Option', 'Sample Option', 'Sample Option']);
-          } else {
-               addFormElement(type, labelInput, placeholderORoptions || 'Sample Placeholder');
-          };
-
-
-          form.reset();
-          form.closest('.component-item').classList.remove('expanded');
-     })
-
-     form.querySelector('.cancel').addEventListener('click', () => {
-          form.reset();
-          form.closest('.component-item').classList.remove('expanded');
-     })
-})
+document.getElementById('copyHtmlBtn').onclick = () => {
+     copyHTML();
+}
 
 
 
